f70f63c40785c480e16c5e80d5e80fe8f4dd891d
Maulana Fauzi <mdedyfauzi@gmail.com>
2023-04-04 06:11:19 +0700

    react_basic_task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState(0);
+  const [variable, setVariable] = useState(0);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setVariable(parseInt(e.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Simplest Wroking Calculator</h1>
+      <p>{result}</p>
+      <input value={variable} onChange={handleChange} />
+      <button onClick={() => setResult((prevResult) => prevResult + parseInt(variable))}>+</button>
+      <button onClick={() => setResult((prevResult) => prevResult - parseInt(variable))}>-</button>
+      <button onClick={() => setResult((prevResult) => prevResult * parseInt(variable))}>x</button>
+      <button onClick={() => setResult((prevResult) => prevResult / parseInt(variable))}>/</button>
+      <button onClick={() => setVariable(0)}>reset input</button>
+      <button onClick={() => setResult(0)}>reset result</button>
     </div>
   );
 }
